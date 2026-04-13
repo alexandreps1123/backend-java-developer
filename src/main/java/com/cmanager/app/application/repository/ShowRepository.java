a6815c93c228626c880dc44405c9e90286aa1566
alexandreps1123 <alexandre.ps1123@gmail.com>
2026-04-13 10:24:50 -0300

    feat: recurso para listagem de shows, com paginação e ordenação por nome

--- src/main/java/com/cmanager/app/application/repository/ShowRepository.java
+++ src/main/java/com/cmanager/app/application/repository/ShowRepository.java
@@ -2,6 +2,8 @@ package com.cmanager.app.application.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,8 @@ import com.cmanager.app.application.domain.Show;
 @Repository
 public interface ShowRepository extends JpaRepository<Show, String> {
     
+    Page<Show> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
     Optional<Show> findByIdIntegration(Integer idIntegration);
     
 }
--- src/main/java/com/cmanager/app/application/service/ShowService.java
+++ src/main/java/com/cmanager/app/application/service/ShowService.java
@@ -1,11 +1,16 @@
 package com.cmanager.app.application.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cmanager.app.application.adapter.ShowAdapter;
+import com.cmanager.app.application.data.ShowDTO;
 import com.cmanager.app.application.domain.Show;
 import com.cmanager.app.application.repository.ShowRepository;
+import com.cmanager.app.core.data.PageResultResponse;
+import com.cmanager.app.core.utils.Util;
 import com.cmanager.app.integration.dto.ShowsRequestDTO;
 
 @Service
@@ -24,6 +29,15 @@ public class ShowService {
     public Show create(ShowsRequestDTO apiShow) {
         return showRepository.findByIdIntegration(apiShow.id())
                 .orElseGet(() -> showRepository.save(showAdapter.toShow(apiShow)));
+    }
+
+    @Transactional(readOnly = true)
+    public PageResultResponse<ShowDTO> list(String name, int page, int size, String sortField, String sortOrder) {
+        final Pageable pageable = Util.getPageable(page, size, sortField, sortOrder);
+        final String filter = name == null ? "" : name;
+        Page<Show> result = showRepository.findByNameContainingIgnoreCase(filter, pageable);
+        Page<ShowDTO> dtoPage = result.map(ShowDTO::convertEntity);
+        return PageResultResponse.from(dtoPage);
     }
 
 }
